0ca801fb50816abeffe2605d5b12bdd52540df22
Tomasra98 <tomasramirezagudelo@gmail.com>
2025-11-09 02:42:37 -0500

    Docker en local funciona completamente

--- encuentros-back/src/main.ts
+++ encuentros-back/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:4200', // Permite solo tu frontend Angular
+    origin: ['http://localhost', 'http://localhost:4200', 'http://localhost:80'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
